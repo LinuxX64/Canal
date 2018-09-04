e67c87b413a14ef447d1c01e85865e90fd8aa15e
Linux <linuxx64@gmail.com>
2018-09-03 22:21:26 -0300

    Editei algumas coisas, e arrumei uns prefixos.

--- Gladiator/src/br/com/linux/Main.java
+++ Gladiator/src/br/com/linux/Main.java
@@ -1,7 +1,21 @@
 package br.com.linux;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	public static Plugin plugin;
+	public static Plugin instance;
+
+	public static String semp = "§c§lERRO §fVocê não tem §3§lACESSO §fa esse comando.";
+
+	public static Plugin getPlugin() {
+		return plugin;
+	}
+
+	public static Plugin getInstance() {
+		return instance;
+	}
 
 }
